5acfa9ebe8127a5679fdcd765242aadb5eac6304
Ben Ehmke <ben@bemky.com>
2012-10-22 09:54:21 -0500

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bemky::Application.config.secret_token = '1aa60c37a6a2899dbc33d5658b68dfb7e3413b92b5a46a9a1eaffbf782b84271498607ddb60ce5c27bb718cd415a133b6c59c41620c40d1850a20fd1c49a041f'
